e3e5e7e788466f19aa276107c6f1db925fbb2d4b
Avi ד <avi.the.coder@gmail.com>
2019-02-07 09:22:32 -0500

    Convert extension.ts to Coc

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import * as vscode from 'vscode';
+import * as vscode from 'coc.nvim';
 import * as lc from 'vscode-languageclient';
 
 import * as commands from './commands';
@@ -7,119 +7,116 @@ import * as events from './events';
 import * as notifications from './notifications';
 import { Server } from './server';
 
-export function activate(context: vscode.ExtensionContext) {
-    function disposeOnDeactivation(disposable: vscode.Disposable) {
-        context.subscriptions.push(disposable);
-    }
+export async function activate(context: vscode.ExtensionContext) {
 
-    function registerCommand(name: string, f: any) {
-        disposeOnDeactivation(vscode.commands.registerCommand(name, f));
-    }
-    function overrideCommand(
-        name: string,
-        f: (...args: any[]) => Promise<boolean>
-    ) {
-        const defaultCmd = `default:${name}`;
-        const original = (...args: any[]) =>
-            vscode.commands.executeCommand(defaultCmd, ...args);
+  // Commands are requests from vscode to the language server
+  vscode.commands.registerCommand(
+    'rust-analyzer.analyzerStatus',
+    commands.analyzerStatus.makeCommand(context)
+  );
+  vscode.commands.registerCommand('rust-analyzer.collectGarbage', () =>
+    Server.client.sendRequest<null>('rust-analyzer/collectGarbage', null)
+  );
+  vscode.commands.registerCommand('rust-analyzer.syntaxTree', commands.syntaxTree.handle);
+  vscode.commands.registerCommand(
+    'rust-analyzer.extendSelection',
+    commands.extendSelection.handle
+  );
+  vscode.commands.registerCommand(
+    'rust-analyzer.matchingBrace',
+    commands.matchingBrace.handle
+  );
+  vscode.commands.registerCommand('rust-analyzer.joinLines', commands.joinLines.handle);
+  vscode.commands.registerCommand('rust-analyzer.parentModule', commands.parentModule.handle);
+  vscode.commands.registerCommand('rust-analyzer.run', commands.runnables.handle);
+  // Unlike the above this does not send requests to the language server
+  vscode.commands.registerCommand('rust-analyzer.runSingle', commands.runnables.handleSingle);
+  vscode.commands.registerCommand(
+    'rust-analyzer.applySourceChange',
+    commands.applySourceChange.handle
+  );
 
-        try {
-            registerCommand(name, async (...args: any[]) => {
-                const editor = vscode.window.activeTextEditor;
-                if (
-                    !editor ||
-                    !editor.document ||
-                    editor.document.languageId !== 'rust'
-                ) {
-                    return await original(...args);
-                }
-                if (!(await f(...args))) {
-                    return await original(...args);
-                }
-            });
-        } catch (_) {
-            vscode.window.showWarningMessage(
-                'Enhanced typing feature is disabled because of incompatibility with VIM extension'
-            );
+  vscode.commands.registerCommand(
+    'rust-analyzer.showReferences',
+    async () => {
+      const position = await vscode.workspace.getCursorPosition()
+      const document = await vscode.workspace.document
+      // FIXME does this work?
+      let locations: lc.Location[] = []
+      vscode.commands.executeCommand(
+        'editor.action.showReferences',
+        document.uri,
+        position,
+        locations
+      );
+    }
+  );
+  function overrideCommand(
+    name: string,
+    f: (...args: any[]) => Promise<boolean>
+  ) {
+    const defaultCmd = `default:${name}`;
+    const original = (...args: any[]) =>
+      vscode.commands.executeCommand(defaultCmd, ...args);
+
+    try {
+      vscode.commands.registerCommand(name, async (...args: any[]) => {
+        const document = await vscode.workspace.document
+        if (
+          !document ||
+          document.filetype !== 'rust'
+        ) {
+          return await original(...args);
         }
-    }
-
-    // Commands are requests from vscode to the language server
-    registerCommand(
-        'rust-analyzer.analyzerStatus',
-        commands.analyzerStatus.makeCommand(context)
-    );
-    registerCommand('rust-analyzer.collectGarbage', () =>
-        Server.client.sendRequest<null>('rust-analyzer/collectGarbage', null)
-    );
-    registerCommand('rust-analyzer.syntaxTree', commands.syntaxTree.handle);
-    registerCommand(
-        'rust-analyzer.extendSelection',
-        commands.extendSelection.handle
-    );
-    registerCommand(
-        'rust-analyzer.matchingBrace',
-        commands.matchingBrace.handle
-    );
-    registerCommand('rust-analyzer.joinLines', commands.joinLines.handle);
-    registerCommand('rust-analyzer.parentModule', commands.parentModule.handle);
-    registerCommand('rust-analyzer.run', commands.runnables.handle);
-    // Unlike the above this does not send requests to the language server
-    registerCommand('rust-analyzer.runSingle', commands.runnables.handleSingle);
-    registerCommand(
-        'rust-analyzer.applySourceChange',
-        commands.applySourceChange.handle
-    );
-    registerCommand(
-        'rust-analyzer.showReferences',
-        (uri: string, position: lc.Position, locations: lc.Location[]) => {
-            vscode.commands.executeCommand(
-                'editor.action.showReferences',
-                vscode.Uri.parse(uri),
-                Server.client.protocol2CodeConverter.asPosition(position),
-                locations.map(Server.client.protocol2CodeConverter.asLocation)
-            );
+        if (!(await f(...args))) {
+          return await original(...args);
         }
-    );
+      });
+    } catch (_) {
+      console.error("overrideCommand does not work")
+    }
+  }
 
-    overrideCommand('type', commands.onEnter.handle);
+  overrideCommand('type', commands.onEnter.handle);
 
-    // Notifications are events triggered by the language server
-    const allNotifications: Iterable<
-        [string, lc.GenericNotificationHandler]
+  // Notifications are events triggered by the language server
+  const allNotifications: Iterable<
+    [string, lc.GenericNotificationHandler]
     > = [
-        [
-            'rust-analyzer/publishDecorations',
-            notifications.publishDecorations.handle
-        ]
+      [
+        'rust-analyzer/publishDecorations',
+        notifications.publishDecorations.handle
+      ]
     ];
 
-    // The events below are plain old javascript events, triggered and handled by vscode
-    vscode.window.onDidChangeActiveTextEditor(
-        events.changeActiveTextEditor.handle
-    );
+  // TODO semantic highlighting
+  // The events below are plain old javascript events, triggered and handled by vscode
+  // vscode.window.onDidChangeActiveTextEditor(
+  //   events.changeActiveTextEditor.handle
+  // );
 
-    const textDocumentContentProvider = new TextDocumentContentProvider();
-    disposeOnDeactivation(
-        vscode.workspace.registerTextDocumentContentProvider(
-            'rust-analyzer',
-            textDocumentContentProvider
-        )
-    );
+  const textDocumentContentProvider = new TextDocumentContentProvider();
+  // TODO for when Coc gets dispose support
+  // disposeOnDeactivation(
+  //   vscode.workspace.registerTextDocumentContentProvider(
+  //     'rust-analyzer',
+  //     textDocumentContentProvider
+  //   )
+  // );
 
-    vscode.workspace.onDidChangeTextDocument(
-        events.changeTextDocument.createHandler(textDocumentContentProvider),
-        null,
-        context.subscriptions
-    );
+  vscode.workspace.onDidChangeTextDocument(
+    events.changeTextDocument.createHandler(textDocumentContentProvider),
+    null,
+    context.subscriptions
+  );
 
-    // Start the language server, finally!
-    Server.start(allNotifications);
+  // Start the language server, finally!
+  Server.start(allNotifications);
 }
 
 export function deactivate(): Thenable<void> {
-    if (!Server.client) {
-        return Promise.resolve();
-    }
-    return Server.client.stop();
+  if (!Server.client) {
+    return Promise.resolve();
+  }
+  return Server.client.stop();
 }
