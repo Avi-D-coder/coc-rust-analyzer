bb463daf593d2c876e24ccca8c54816665771a43
Avi ד <avi.the.coder@gmail.com>
2019-02-09 23:17:27 -0500

    Don't support viewing syntax Tree

--- src/commands/syntaxTree.ts
+++ src/commands/syntaxTree.ts
@@ -1,50 +1,51 @@
-import * as vscode from 'vscode';
-import { TextDocumentIdentifier } from 'vscode-languageclient';
+// import * as vscode from 'coc.nvim';
+// import { TextDocumentIdentifier } from 'vscode-languageserver-types';
 
-import { Server } from '../server';
+// import Uri from 'vscode-uri';
+// import { Server } from '../server';
 
-export const syntaxTreeUri = vscode.Uri.parse('rust-analyzer://syntaxtree');
+// export const syntaxTreeUri = Uri.parse('rust-analyzer://syntaxtree');
 
-export class TextDocumentContentProvider
-    implements vscode.TextDocumentContentProvider {
-    public eventEmitter = new vscode.EventEmitter<vscode.Uri>();
-    public syntaxTree: string = 'Not available';
+// export class TextDocumentContentProvider
+//     implements vscode.TextDocumentContentProvider {
+//     public eventEmitter = new vscode.EventEmitter<vscode.Uri>();
+//     public syntaxTree: string = 'Not available';
 
-    public provideTextDocumentContent(
-        uri: vscode.Uri
-    ): vscode.ProviderResult<string> {
-        const editor = vscode.window.activeTextEditor;
-        if (editor == null) {
-            return '';
-        }
-        const request: SyntaxTreeParams = {
-            textDocument: { uri: editor.document.uri.toString() }
-        };
-        return Server.client.sendRequest<SyntaxTreeResult>(
-            'rust-analyzer/syntaxTree',
-            request
-        );
-    }
+//     public async provideTextDocumentContent(
+//         uri: Uri
+//     ): Promise<vscode.ProviderResult<string>> {
+//         const document = vscode.workspace.document;
+//         if (document == null) {
+//             return '';
+//         }
+//         const request: SyntaxTreeParams = {
+//             textDocument: { uri: editor.document.uri.toString() }
+//         };
+//         return Server.client.sendRequest<SyntaxTreeResult>(
+//             'rust-analyzer/syntaxTree',
+//             request
+//         );
+//     }
 
-    get onDidChange(): vscode.Event<vscode.Uri> {
-        return this.eventEmitter.event;
-    }
-}
+//     get onDidChange(): vscode.Event<vscode.Uri> {
+//         return this.eventEmitter.event;
+//     }
+// }
 
-interface SyntaxTreeParams {
-    textDocument: TextDocumentIdentifier;
-}
+// interface SyntaxTreeParams {
+//     textDocument: TextDocumentIdentifier;
+// }
 
-type SyntaxTreeResult = string;
+// type SyntaxTreeResult = string;
 
-// Opens the virtual file that will show the syntax tree
-//
-// The contents of the file come from the `TextDocumentContentProvider`
-export async function handle() {
-    const document = await vscode.workspace.openTextDocument(syntaxTreeUri);
-    return vscode.window.showTextDocument(
-        document,
-        vscode.ViewColumn.Two,
-        true
-    );
-}
+// // Opens the virtual file that will show the syntax tree
+// //
+// // The contents of the file come from the `TextDocumentContentProvider`
+// export async function handle() {
+//     const document = await vscode.workspace.openTextDocument(syntaxTreeUri);
+//     return vscode.window.showTextDocument(
+//         document,
+//         vscode.ViewColumn.Two,
+//         true
+//     );
+// }
